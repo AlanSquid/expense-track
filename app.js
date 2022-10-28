e9dd01a84324fc680d870da53fe59a920dffa13c
AlanSquid <squid78313@gmail.com>
2022-10-28 17:14:37 +0800

    setting express and express-hbs

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const PORT = 3000
+
+const app = express()
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
