2a55c6bd1be577df6eaec628730cc123204ff07a
AlanSquid <squid78313@gmail.com>
2022-11-02 02:00:25 +0800

    add expense & user Schema

---
+++ models/expense.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const expenseSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true }
+})
+
+module.exports = mongoose.model('Expense', expenseSchema)
