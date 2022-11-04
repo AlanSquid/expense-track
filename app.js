6b0fd8f2e3f399846cc1603ba72f08718fd32be6
AlanSquid <squid78313@gmail.com>
2022-11-04 17:47:07 +0800

    add passport and passport-local

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -18,6 +20,14 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+usePassport(app)
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,20 +1,44 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+const passport = require('passport')
+
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.render('login')
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: "/",
+  failureRedirect: '/users/login',
+  // if(!username || !password)的情況下預設會回傳Missing credentials，以下更改
+  badRequestMessage: '帳號密碼不能為空白',
+  failureFlash: true
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
 router.post('/register', (req, res) => {
-  res.render('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('此信箱已經註冊過')
+      }
+      User.create(req.body)
+        .then(() => res.redirect('/'))
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/logout', (req, res, next) => {
+  req.logout(err => {
+    if (err) { return next(err) }
+    req.flash('success_msg', '您已成功登出。')
+    res.redirect('/users/login')
+  })
 })
 
 module.exports = router
